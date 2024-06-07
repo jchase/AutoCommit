5fd8e769b22ca95b9b97163364f5b08fa0758e72
Jason Chase <jchase@solidmark.com>
2024-06-07 13:05:51 -0400

    * refactor: Update prompt instructions for commit messages

--- src/autocommit/utils/completion.ts
+++ src/autocommit/utils/completion.ts
@@ -7,14 +7,14 @@
 import {
   ChatCompletionRequestMessage,
   ChatCompletionRequestMessageRoleEnum,
-} from 'openai';
+} from "openai";
 
 const initMessagesPrompt: Array<ChatCompletionRequestMessage> = [
   {
     role: ChatCompletionRequestMessageRoleEnum.System,
-    content: `Compose a succinct and informal commit message following this format: "<type>(<scope>): <description>". The "type" represents the purpose of the commit and can be: 1. feat (a new feature or significant change), 2. fix (a bug fix), 3. docs (documentation changes), 4. style (code style changes), 5. refactor (code refactoring), 6. test (test modifications), 7. chore (routine tasks or maintenance). The "scope" (optional) indicates the project area affected. The "description" should be a brief, four-word maximum summary of the changes, written in present tense.
+    content: `You are provided with a git diff from which to infer a git commit message. Compose a succinct commit message following this format: "<type>: <description>". The "type" represents the purpose of the commit and can be: 1. feat (a new feature or significant change), 2. fix (a bug fix), 3. docs (documentation changes), 4. style (code style changes), 5. refactor (code refactoring), 6. test (test modifications), 7. chore (routine tasks or maintenance). For the "description", infer and identify any and all distinct functional changes that were made, one line per concern affected. Whereever possible, try to infer the most likely purpose of the changes made and use that for the message. Include specific key details where possible and avoid being vague. Descriptions should be a brief, eight-word maximum summary of the changes, written in present tense.
 
-Based on the 'git diff --staged' output I give you, translate it into a commit message. Your response should strictly be the commit message in English. Avoid extraneous text and keep it easily comprehensible.`,
+Based on the 'git diff --staged' output I give you, translate it into a commit message. Your response should strictly be the commit message in English. Keep it easily comprehensible.`,
   },
   {
     role: ChatCompletionRequestMessageRoleEnum.User,
@@ -38,8 +38,8 @@ index ad4db42..f3b18a9 100644
   },
   {
     role: ChatCompletionRequestMessageRoleEnum.Assistant,
-    content: `refactor(header.js): updated 'Old Title' to 'New Title'
-    feat(signup.js): renamed 'Sign Up' to 'Register'`,
+    content: `refactor(header.js): Typographic updates to header
+    feat(signup.js): Typographic updates to Register button`,
   },
 ];
 
