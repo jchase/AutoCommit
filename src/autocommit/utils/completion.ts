3086e2c12267e0e2defed7a4cae87943a28563bc
Jason Chase <jchase@solidmark.com>
2024-06-07 13:26:41 -0400

    * docs: Expands commit message guidelines in completion.ts

--- src/autocommit/utils/completion.ts
+++ src/autocommit/utils/completion.ts
@@ -12,9 +12,28 @@ import {
 const initMessagesPrompt: Array<ChatCompletionRequestMessage> = [
   {
     role: ChatCompletionRequestMessageRoleEnum.System,
-    content: `You are provided with a git diff from which to infer a git commit message. Compose a succinct commit message following this format: "<type>: <description>". The "type" represents the purpose of the commit and can be: 1. feat (a new feature or significant change), 2. fix (a bug fix), 3. docs (documentation changes), 4. style (code style changes), 5. refactor (code refactoring), 6. test (test modifications), 7. chore (routine tasks or maintenance). For the "description", infer and identify any and all distinct functional changes that were made, one line per concern affected. Whereever possible, try to infer the most likely purpose of the changes made and use that for the message. Include specific key details where possible and avoid being vague. Descriptions should be a brief, eight-word maximum summary of the changes, written in present tense.
+    content: `You are provided with a git diff from which to infer a git commit message. Compose a succinct commit message following this format: "<type>: <description>". The "type" represents the purpose of the commit and can be: 1. feat (a new feature or significant change), 2. fix (a bug fix), 3. docs (documentation changes), 4. style (code style changes), 5. refactor (code refactoring), 6. test (test modifications), 7. chore (routine tasks or maintenance). For the "description", infer and identify any and all distinct functional changes that were made, one line per concern affected. Whereever possible, try to infer the most likely purpose of the changes made and use that for the message. Include specific key details where possible and avoid being vague. Descriptions should be a brief, eight-word maximum summary of the changes, written in present tense. 
+    
+    If the diff is clearly a change in version numbers of any kind, you should include that in the message (e.g.: "chore: Bump version to 1.0.0"). A type of "refactor" should only be applied where it is clearly a code refactoring, otherwise, if the change has only to do with verbiage or copy, it should be categorized as a "chore". 
 
-Based on the 'git diff --staged' output I give you, translate it into a commit message. Your response should strictly be the commit message in English. Keep it easily comprehensible.`,
+    The commit message should always try to answer the question "What does this changeset do"? The commit message that follows should be in the format of an implied "Answer: It", i.e. "It... (complete the phrasing with your commit message)". Here are some examples of good commit messages:
+
+    'feat: Adds support for youtube video embedding'
+    'fix: Fixes login bug regarding invalid email addresses'
+    'docs: Updates README with new instructions'
+    'style: Corrects indentation'
+    'feat: Implements Bizzabo API'
+    'refactor: Simplifies header display logic'
+    'test: Adds tests for signup'
+
+    Note specifically that instead of using the words "Add...", "Fix...", "Update...", "Correct...", "Implement...", etc, you should use the phrasing "Adds...", "Fixes...", "Updates...", "Corrects...", "Implements...", etc.
+
+    A few minor exceptions to this rule would be in these instances:
+    'chore: Bump version to 1.0.0'
+    'chore: Typographic edits to header'
+    'chore: Code tidy'
+
+    Based on the 'git diff --staged' output I give you, translate it into a commit message. Your response should strictly be the commit message in English. Keep it easily comprehensible.`,
   },
   {
     role: ChatCompletionRequestMessageRoleEnum.User,
@@ -38,8 +57,8 @@ index ad4db42..f3b18a9 100644
   },
   {
     role: ChatCompletionRequestMessageRoleEnum.Assistant,
-    content: `refactor(header.js): Typographic updates to header
-    feat(signup.js): Typographic updates to Register button`,
+    content: `chore: Typographic updates to header
+    chore: Typographic updates to Register button`,
   },
 ];
 
